d8f73a9256f78380da9275aa8fcb397f04e461e4
krithika1811 <krithika1811@gmail.com>
2023-03-30 16:50:27 -0700

    Udacity-mono-to-microservices

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a2e47939b6a774c14a1259ccba2ddbc6-1828082366.us-east-1.elb.amazonaws.com'
 };
 
 /*
